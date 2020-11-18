73e12d31e3601ff1e105cef3bcd8b0dc72653e74
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2020-11-18 00:04:33 -0300

    Programa Do While

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Você deseja receber um bom dia?");
+
+            string resposta = "";
+
+            int numeroDeVezes = 0;
+
+            do
+            {
+                //O programa executa pelo menos uma vez
+                Console.WriteLine("Bom dia");
+
+                Console.WriteLine("Ainda deseja receber bom dia?");
+
+                numeroDeVezes++;
+
+                resposta = Console.ReadLine();
+
+            } while(resposta == "Sim");
+
+            Console.WriteLine($"Numero de vezes: {numeroDeVezes}");
+            Console.WriteLine("Fim do programa");
+        }
+    }
+}
